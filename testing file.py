96c63ff2fa650ef5859b7772288b6c91eaed48bc
bakerkai <bakerkai@urbandaleschools.com>
2022-01-28 13:51:38 -0600

    what is this

--- testing file.py
+++ testing file.py
@@ -1,14 +1,14 @@
 class Character():
-    def __init__(self):
-        self.name=""
-        self.status=""
+    def __init__(self, new_name, new_status, new_home):
+        self.new_name=""
+        self.new_status=""
         self.strength = 0
         self.midichlorian_count = 0
-        self.home = ""
+        self.new_home = ""
     def fight(self):
-        print("lightsaber on!, says " + self.name)
+        print("lightsaber on!, says " + self.new_name + "," + self.new_status + " from" + self.new_home)
 
-jedi1 = Character()
+jedi1 = Character("Obi Wan","Jedi Master", "Tatooine")
 
 jedi1.name = "Obi Wan"
 jedi1.status = "Jedi Master"
@@ -16,7 +16,7 @@ jedi1.strength = 100
 jedi1.midichlorian_count = 20
 jedi1.home = "Tatooine"
 
-jedi2 = Character()
+jedi2 = Character("Yoda", "Jedi Master", "Tatooine")
 
 jedi2.name = "Yoda"
 jedi2.status = "Jedi Master"
